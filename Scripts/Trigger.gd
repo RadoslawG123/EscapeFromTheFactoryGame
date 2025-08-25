d5d0ea98084298727afa0c10d5aac9610fe2a596
Radosław Górecki <gorecki208@onet.pl>
2025-08-25 23:09:24 +0200

    Rebuild Trigger for ConveyorBelt moving objects/obstacles. Add obstacles. Level Design.

--- Scripts/Trigger.gd
+++ Scripts/Trigger.gd
@@ -1,31 +1,64 @@
 extends Node2D
 
-@export var scene_pos: Dictionary[Vector2, PackedScene]
-
-var inst
-
-func _process(delta: float) -> void:
-	print("Current_Scene: ", get_tree().current_scene)
-	if inst != null:
-		print("Child added? ", inst.get_parent())
-		print("Position: ", inst.global_position)
-		print(inst.name)
-
 func _on_trigger_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		spawn_objects()
 
 func spawn_objects():
-	print("Triggered")
-	for item in scene_pos:
-		inst = scene_pos[item].instantiate()
-		#print("Inst: ", inst)
-		call_deferred("_add_instance", inst, item)
+	for child in get_children():
+		var activate = child.get("activate")
 		
+		if activate != null:
+			child.set("activate", true)
+		else:
+			print("Activate variable not found in child: ", child)
 
-func _add_instance(inst, pos):
-	get_tree().current_scene.add_child(inst)
-	inst.position = pos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#@export var scene_pos: Dictionary[Vector2, PackedScene]
+
+#var inst
+
+#func _process(delta: float) -> void:
 	#print("Current_Scene: ", get_tree().current_scene)
-	#print("Child added? ", inst.get_parent())
-	#print("Position: ", inst.global_position)
+	#if inst != null:
+		#print("Child added? ", inst.get_parent())
+		#print("Position: ", inst.global_position)
+		#print(inst.name)
+
+#func _on_trigger_entered(body: Node2D) -> void:
+	#if body.name == "Player":
+		#spawn_objects()
+
+#func spawn_objects():
+	#print("Triggered")
+	#for item in scene_pos:
+		#inst = scene_pos[item].instantiate()
+		##print("Inst: ", inst)
+		#call_deferred("_add_instance", inst, item)
+		#
+#
+#func _add_instance(inst, pos):
+	#get_tree().current_scene.add_child(inst)
+	#inst.position = pos
+	##print("Current_Scene: ", get_tree().current_scene)
+	##print("Child added? ", inst.get_parent())
+	##print("Position: ", inst.global_position)
+	
+	
--- Scripts/obstacles.gd
+++ Scripts/obstacles.gd
@@ -1,4 +1,16 @@
-extends CharacterBody2D
+extends AnimatableBody2D
+
+@export var velocity: float = 0.0
+@export var startPositionX: float = 0.0
+@export var endPositionX: float = 0.0
+@export var activate := false
+
+@onready var ob: AnimatableBody2D = $"."
+
 
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	if activate:
+		ob.position.x -= velocity
+		
+	if ob.position.x <= endPositionX:
+		ob.position.x = startPositionX
